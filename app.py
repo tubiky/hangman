0246c4a2141012993bf4a11f1ba24718db564b85
tubiky <61419018+tubiky@users.noreply.github.com>
2020-11-02 22:32:34 +0900

    Succeeded in displaying ANSWER at random

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
-import pygame as pg
+import pygame
 import random
-
-
 
 # 파일을 화면에 끓어다 놓으면 해당 파일을 parameter로 받는 function으로 개선
 def check_voca_list():
-    DATA = []
+    
+    VOCA = []
 
     with open ('words.txt', 'r') as words:
-        DATA.append(words.readlines())
+        DATA = words.readlines()
+    
+    for d in DATA:
+        VOCA.append(d.strip())
 
-    print(DATA)
+    print(VOCA)
+    return VOCA
 
 def randomize_answer(words):
-    no = random.randint(0, len(words))
-    answer = words[no]
+    answer = ''
+
+    if words != False:
+        random.shuffle(words)
+        answer = words.pop(0)
 
     return answer
 
 def main():
 
     DUMMY_DATA = ["apple", "orange", "lemon", "watermelon"]
-
-    screen = pg.display.set_mode((640, 480))
-    font = pg.font.Font(None, 32)
-    clock = pg.time.Clock()
+    WIDTH, HEIGHT = 640, 480
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    font = pygame.font.Font(None, 32)
+    clock = pygame.time.Clock()
     
-    input_box = pg.Rect(100, 100, 140, 32)
-    answer_box = pg.Rect(10, 10, 200, 64)
+    answer_box = pygame.Rect(WIDTH * 0.1, HEIGHT * 0.1, WIDTH * 0.8, HEIGHT / 4)
+    input_box = pygame.Rect(WIDTH * 0.1, HEIGHT * 0.4, WIDTH * 0.8, HEIGHT / 6)
 
-    color_inactive = pg.Color('lightskyblue3')
-    color_active = pg.Color('dodgerblue2')
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
     color = color_inactive
     active = False
     text = ''
@@ -38,11 +44,11 @@ def main():
     done = False
 
     while not done:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 check_voca_list()
                 done = True
-            if event.type == pg.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 # If the user clicked on the input_box rect.
                 if input_box.collidepoint(event.pos):
                     # Toggle the active variable.
@@ -52,17 +58,21 @@ def main():
                 # Change the current color of the input box.
                 color = color_active if active else color_inactive
 
-            if event.type == pg.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                 if active:
-                    if event.key == pg.K_RETURN:
-                        print(text)
-                        text = ''
+                    if event.key == pygame.K_RETURN:
+                        if text == answer:
+                            print("Correct!")
+                            text = ''
+                        else:
+                            print("Wrong!")
+                            text = ''
 
-                    elif event.key == pg.K_BACKSPACE:
+                    elif event.key == pygame.K_BACKSPACE:
                         text = text[:-1]
 
-                    elif event.key == pg.K_F1:
-                        answer = randomize_answer(DUMMY_DATA)
+                    elif event.key == pygame.K_F1:
+                        answer = randomize_answer(check_voca_list())
 
                     else:
                         text += event.unicode
@@ -75,23 +85,23 @@ def main():
         txt_answer = font.render(answer, True, color)
 
         # Resize the box if the text is too long.
-        width = max(200, txt_surface.get_width()+10)
+        width = max(400, txt_surface.get_width()+10)
         input_box.w = width
         # Blit the text.
         screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
         # Blit the input_box rect.
-        pg.draw.rect(screen, color, input_box, 2)
+        pygame.draw.rect(screen, color, input_box, 2)
         
         # Blit the answer_box rect.
-        pg.draw.rect(screen, color, answer_box, 2)
+        pygame.draw.rect(screen, color, answer_box, 2)
         screen.blit(txt_answer, (answer_box.x+5, answer_box.y+5))
 
-        pg.display.flip()
+        pygame.display.flip()
 
         clock.tick(30)
 
 
 if __name__ == '__main__':
-    pg.init()
+    pygame.init()
     main()
-    pg.quit()
+    pygame.quit()
